dd1530546e63aa55160281eb3f230c6c7c8b74bb
Divya Dhiman <dhimandivya47@gmail.com>
2025-07-17 12:50:43 +0530

    notezy

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,7 @@
  const mongoose = require('mongoose');
 
-//to connect the mongoose with the uri
-// const URI = "mongodb+srv://divya:DivyaDhiman220902@cluster0.vp0xyfe.mongodb.net/admin_panel?appName=mongosh+2.5.0"
 const URI = process.env.MONGODB_URI;
 
-
-//mongoose.connect(URI);
 
 const connectDB = async ()=>{
     try {
--- backend/utils/sendOTP.js
+++ backend/utils/sendOTP.js
@@ -1,12 +1,12 @@
-// utils/sendOTP.js
+
 const nodemailer = require("nodemailer");
 
 const sendOTP = async (email, otp) => {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
-      user: process.env.GMAIL_USER,       // Must be your Gmail
-      pass: process.env.GMAIL_PASS,       // App password (not your Gmail password)
+      user: process.env.GMAIL_USER,       
+      pass: process.env.GMAIL_PASS,      
     },
   });
 
